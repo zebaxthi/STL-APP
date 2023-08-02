57039072de05a9c0dafcd0e9cfa4787128940f1e
Sebastian Aguirre <sebastian.aguirre@wiga.io>
2023-08-01 23:51:17 -0500

    Pantalla de WelcomeActivity

---
+++ app/src/main/java/com/uco/stlapp/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package com.uco.stlapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
