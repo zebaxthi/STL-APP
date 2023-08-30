c1a32bd5c34fdc5442296949ffc9b10eba5277cd
pablo <pablo33011@gmail.com>
2023-08-30 18:18:21 -0500

    Article Provider Data Update

--- app/src/main/java/com/uco/stlapp/providers/ArticleProvider.kt
+++ app/src/main/java/com/uco/stlapp/providers/ArticleProvider.kt
@@ -5,22 +5,22 @@ import com.uco.stlapp.models.Article
 class ArticleProvider {
     companion object{
         val articleList = listOf<Article>(
-            Article("Spiderman", "Marvel", 4, "w", null),
-            Article("Daredevil", "Marvel", 3, "q", null),
-            Article("Wolverine", "Marvel", 2, "s", null),
-            Article("Batman", "DC", 4, "a", null),
-            Article("Thor", "Marvel", 2, "s", null),
-            Article("Flash", "DC", 5, "f", null),
-            Article("Green Lantern", "DC", 1, "a", null),
-            Article("Wonder Woman", "DC", 8, "f", null),
-            Article("Spiderman", "Marvel", 4, "w", null),
-            Article("Daredevil", "Marvel", 3, "q", null),
-            Article("Wolverine", "Marvel", 2, "s", null),
-            Article("Batman", "DC", 4, "a", null),
-            Article("Thor", "Marvel", 2, "s", null),
-            Article("Flash", "DC", 5, "f", null),
-            Article("Green Lantern", "DC", 1, "a", null),
-            Article("Wonder Woman", "DC", 8, "f", null),
+            Article("Multímetro", "15XP-A", 4, "Bueno", null),
+            Article("Multímetro LCR", "LCR55A", 2, "Bueno", null),
+            Article("Puntas de Multímetro", "20A 1000V", 17, "Bueno", null),
+            Article("Fuente", "Utp1306", 4, "Regular", null),
+            Article("Puntas de Fuente", "10 mA 110V", 2, "Bueno", null),
+            Article("Cable de Poder", "110/240V 5A", 21, "Bueno", null),
+            Article("Punta de Osciloscopio", "CLEQEE", 4, "Regular", null),
+            Article("Router", "1941w-a/k9", 3, "Bueno", null),
+            Article("Cable UTP", "Category 5", 16, "Bueno", null),
+            Article("Ponchadora", "11212530", 2, "Bueno", null),
+            Article("Motor con Reductor", "R130", 27, "Bueno", null),
+            Article("Cable de Consola", "DB9 a RJ45", 4, "Bueno", null),
+            Article("Pantalla LCD", "LCD1602", 3, "Regular", null),
+            Article("Arduino", "UNO", 3, "Bueno", null),
+            Article("Protoboard", "MB 10-2", 1, "Malo", null),
+            Article("USB B Arduino", "CAB-2716", 3, "Bueno", null),
         )
     }
 }
