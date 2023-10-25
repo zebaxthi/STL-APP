add502d42cf114458e6212d736efe863e76d0e28
Sebastian Aguirre <sebastian.aguirre@wiga.io>
2023-10-25 18:48:13 -0500

    bug in menu

--- app/src/main/java/com/uco/stlapp/services/WebService.kt
+++ app/src/main/java/com/uco/stlapp/services/WebService.kt
@@ -4,8 +4,13 @@ import com.uco.stlapp.models.ArticleResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
 
 interface WebService {
     @GET("/crfhKA/articles")
     suspend fun getArticles(): Response<List<ArticleResponse>>
+
+    @PATCH("/crfhKA/articles/{id}")
+    suspend fun patchArticle(@Path("id") id: Int): Response<ArticleResponse>
 }
